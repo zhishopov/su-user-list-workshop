c53d8dd514e065a29cccff9a91c380c1f22550f7
Zhivko Shopov <zhishopov@gmail.com>
2025-03-13 20:11:19 +0000

    Add Footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import "./styles.css";
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Header></Header>
+      <Footer></Footer>
     </>
   );
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <>
+      <footer class="footer">
+        <p>
+          This site is designed to be used for training purposes at SoftUni.
+        </p>
+      </footer>
+    </>
+  );
+}
