07ddefb1f5cc59bd2fa3870255c3a855127f6422
Zhivko Shopov <zhishopov@gmail.com>
2025-03-13 19:56:10 +0000

    Add Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,12 @@
-function App() {}
+import Header from "./components/Header";
+import "./styles.css";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+}
 
 export default App;
